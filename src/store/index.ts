91904cb652c6a2c035aa8416171b160714eafa22
FEDGE 2.O <103391215+cryptofedge@users.noreply.github.com>
2026-04-29 04:28:25 -0400

    fix: src/store/index.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import playerReducer from './playerSlice';
 import gameReducer from './gameSlice';
+import labelReducer from './labelSlice';
 
 export const store = configureStore({
   reducer: {
     player: playerReducer,
     game: gameReducer,
+    label: labelReducer,
   },
 });
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof 
